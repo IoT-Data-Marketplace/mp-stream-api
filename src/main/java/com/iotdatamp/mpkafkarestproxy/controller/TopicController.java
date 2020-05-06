bda0af74f3a62758a7b7bfb25ec0ae50db16ef0f
ddanijel <danijel.fon@gmail.com>
2020-05-06 22:21:46 +0200

    RequiredArgsConstructor

--- src/main/java/com/iotdatamp/mpkafkarestproxy/controller/TopicController.java
+++ src/main/java/com/iotdatamp/mpkafkarestproxy/controller/TopicController.java
@@ -3,6 +3,7 @@ package com.iotdatamp.mpkafkarestproxy.controller;
 import com.iotdatamp.mpkafkarestproxy.HttpResponseDTO;
 import com.iotdatamp.mpkafkarestproxy.dto.CreateTopicDTO;
 import com.iotdatamp.mpkafkarestproxy.service.TopicService;
+import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,13 +12,10 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/topics")
+@RequiredArgsConstructor
 public class TopicController {
 
     private final TopicService topicService;
-
-    public TopicController(TopicService topicService) {
-        this.topicService = topicService;
-    }
 
     @SneakyThrows
     @PostMapping
